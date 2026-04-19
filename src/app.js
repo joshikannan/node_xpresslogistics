a31f8da01168fef8da465bc5cbc9fc6fdc223102
joshikannan <joshikannan99@gmail.com>
2026-04-19 17:34:17 +0530

    checkAutoDeploy

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use("/api/tenants", tenantRoutes);
 app.use("/api/customers", customerRoutes);
 
 app.get("/", (req, res) => {
-  res.send("check the manual deployment");
+  res.send("check the auto deployment");
 });
 
 export default app;
